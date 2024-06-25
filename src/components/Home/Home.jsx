224577711fbca7974e8b6d490b70f7b5fc5dcdb4
mizan21 <mizanorrahman398@gmail.com>
2024-06-25 16:45:47 +0600

    Elevate part added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Others/Banner/Banner";
+import Elevate from "../Others/Elevate/Elevate";
 import IntroVideo from "../Others/IntroVideo/IntroVideo";
 import ServicesCarousel from "../Others/ServicesCarousel/ServicesCarousel";
 import Work from "../Others/Work/Work";
@@ -9,7 +10,8 @@ const Home = () => {
       <Banner></Banner>
       <IntroVideo></IntroVideo>
       <Work></Work>
-      <ServicesCarousel></ServicesCarousel>
+      <Elevate></Elevate>
+      {/* <ServicesCarousel></ServicesCarousel> */}
     </div>
   );
 };
---
+++ src/components/Others/Elevate/Elevate.jsx
@@ -0,0 +1,35 @@
+import bg from "../../../assets/images/cta-bg.png";
+const Elevate = () => {
+  const styles = {
+    display: "flex",
+    justifyContent: "center",
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover", // Adjust based on your preference
+    backgroundPosition: "center", // Adjust based on your preference
+  };
+  return (
+    <div className="lg:max-w-[1000px] lg:container lg:mx-auto mx-5 my-10">
+      <div
+        style={styles}
+        className="flex justify-center items-center min-h-60 lg:h-[30vh] rounded-badge"
+      >
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 px-10">
+          <h2 className="text-4xl lg:text-6xl font-sans font-medium text-white">
+            Elevate Your Brand Today!
+          </h2>
+          <div>
+            <p className="text-white">
+              Ready to transform your digital presence? Let&apos;s create magic
+              together! book our services now!
+            </p>
+            <button className="bg-white rounded-full mt-5 px-6 py-2 text-[#185C5D] font-semibold">
+              Book A Call
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Elevate;
