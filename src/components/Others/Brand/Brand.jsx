d6c06209a8262a4cccd7fdad9abc473abe2cdeee
mizan21 <mizanorrahman398@gmail.com>
2024-06-26 13:47:18 +0600

    Brand part added

--- src/components/Others/Brand/Brand.jsx
+++ src/components/Others/Brand/Brand.jsx
@@ -1,38 +1,122 @@
-import Slider from "react-slick";
 import "./Brand.css";
+import brand1 from "../../../assets/images/l-1.png";
+import brand2 from "../../../assets/images/l-2.png";
+import brand3 from "../../../assets/images/l-3.png";
+import brand5 from "../../../assets/images/l-5.png";
+import brand6 from "../../../assets/images/l-6.png";
+import brand7 from "../../../assets/images/l-7.png";
+import brand8 from "../../../assets/images/l-8.png";
+import brand10 from "../../../assets/images/l-10.png";
+import brand12 from "../../../assets/images/l-12.png";
+import brand13 from "../../../assets/images/l-13.png";
+import brand14 from "../../../assets/images/l-14.png";
+import brand17 from "../../../assets/images/l-17.png";
+import brand18 from "../../../assets/images/l-18.png";
+
 const Brand = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-  };
   return (
-    <div className="slider-container container mx-auto">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+    <div className="my-20 lg:container lg:mx-auto">
+      <h3 className="text-center mb-10 text-4xl font-medium text-[#185C5D]">
+        Our Beloved Brands
+      </h3>
+      <div className="carousel-wrapper">
+        <div className="carousel-container">
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand1}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand2}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand3}
+              alt="Drink"
+            />
+          </div>
+
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand5}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand6}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand7}
+              alt="Drink"
+            />
+          </div>
+
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand10}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand12}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand8}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand13}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand14}
+              alt="Drink"
+            />
+          </div>
+
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand17}
+              alt="Drink"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-20 hover:saturate-100"
+              src={brand18}
+              alt="Drink"
+            />
+          </div>
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+      </div>
     </div>
   );
 };
--- src/components/Others/ServicesCarousel/ServicesCarousel.jsx
+++ src/components/Others/ServicesCarousel/ServicesCarousel.jsx
@@ -1,13 +1,15 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./custom.css";
 
 const ServicesCarousel = () => {
   var settings = {
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
