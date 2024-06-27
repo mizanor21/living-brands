72545f58aace86b40e3da965d389b2bd7a8fb59a
mizan21 <mizanorrahman398@gmail.com>
2024-06-27 16:13:58 +0600

    updated brands

--- src/components/Others/Brand/Brand.jsx
+++ src/components/Others/Brand/Brand.jsx
@@ -19,100 +19,192 @@ const Brand = () => {
       <h3 className="text-center mb-10 text-4xl font-semibold text-[#185C5D]">
         Our Beloved Brands
       </h3>
-      <div className="carousel-wrapper">
-        <div className="carousel-container gap-5">
+      <div className="carousel-wrapper mb-5">
+        <div className="carousel-container carousel-container-1">
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand1}
-              alt="Drink"
+              alt="Brand 1"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand2}
-              alt="Drink"
+              alt="Brand 2"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand3}
-              alt="Drink"
+              alt="Brand 3"
             />
           </div>
-
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand5}
-              alt="Drink"
+              alt="Brand 5"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand6}
-              alt="Drink"
+              alt="Brand 6"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand7}
-              alt="Drink"
+              alt="Brand 7"
             />
           </div>
-
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand10}
-              alt="Drink"
+              alt="Brand 10"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand12}
-              alt="Drink"
+              alt="Brand 12"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand8}
-              alt="Drink"
+              alt="Brand 8"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand13}
-              alt="Drink"
+              alt="Brand 13"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand14}
-              alt="Drink"
+              alt="Brand 14"
             />
           </div>
-
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand17}
-              alt="Drink"
+              alt="Brand 17"
             />
           </div>
           <div className="carousel-item">
             <img
-              className="saturate-0 h-20 hover:saturate-100"
+              className="saturate-0 h-10 hover:saturate-100"
               src={brand18}
-              alt="Drink"
+              alt="Brand 18"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="carousel-wrapper">
+        <div className="carousel-container carousel-container-2">
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand1}
+              alt="Brand 1"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand2}
+              alt="Brand 2"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand3}
+              alt="Brand 3"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand5}
+              alt="Brand 5"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand6}
+              alt="Brand 6"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand7}
+              alt="Brand 7"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand10}
+              alt="Brand 10"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand12}
+              alt="Brand 12"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand8}
+              alt="Brand 8"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand13}
+              alt="Brand 13"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand14}
+              alt="Brand 14"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand17}
+              alt="Brand 17"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="saturate-0 h-10 hover:saturate-100"
+              src={brand18}
+              alt="Brand 18"
             />
           </div>
         </div>
