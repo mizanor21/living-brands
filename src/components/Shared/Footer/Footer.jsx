92705fc78bb0f5fe506e30814b7d320eed77632d
mizan21 <mizanorrahman398@gmail.com>
2024-06-30 12:00:50 +0600

    Some code changes

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -7,56 +7,70 @@ import {
   FaRegCopyright,
   FaYoutube,
 } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-[#185C5D] ">
       <footer className="footer text-base-content p-10 items-center justify-center">
         <img className="max-h-[230px] max-w-[900px]" src={logo} alt="" />
       </footer>
-      <footer className="footer block lg:flex lg:justify-between border-base-300 border-t px-10 py-4 text-white justify-center">
-        <div className="flex justify-center items-center gap-4 text-3xl">
-          <a
-            href="https://www.facebook.com/Livingbrands.co"
+      <footer className="footer block lg:flex lg:justify-between border-base-300 border-b px-10 py-4 text-white justify-center">
+        <div className="flex justify-center items-center gap-3 text-3xl">
+          <Link
+            to="https://www.facebook.com/Livingbrands.co"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaFacebook />
-          </a>
-          <a
-            href="https://www.instagram.com/livingbrands.co/"
+          </Link>
+          <Link
+            to="https://www.instagram.com/livingbrands.co/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaInstagramSquare />
-          </a>
-          <a
-            href="https://www.youtube.com/@livingbrands862"
+          </Link>
+          <Link
+            to="https://www.youtube.com/@livingbrands862"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaYoutube />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/livingbrandsbyhabson/"
+          </Link>
+          <Link
+            to="https://www.linkedin.com/company/livingbrandsbyhabson/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaLinkedin />
-          </a>
+          </Link>
         </div>
         <div className="mt-5">
           <span className="flex items-center gap-2">
-            <CiLocationOn className="text-2xl" />{" "}
+            <CiLocationOn className="text-3xl" />{" "}
             <p>
               Hosue: 230, New D.O.H.S Lake Road, Lane-16, Mohakhali, Dhaka-1206.
             </p>
           </span>
           <div className=" flex items-center justify-end gap-2">
             <FaRegCopyright className="text-xl" />
-            <p>Copyright Lemmesay Ltd. All Rights Reserved</p>
+            <p>Living Brands All Rights Reserved</p>
           </div>
         </div>
       </footer>
+      <div className="flex justify-center text-white py-5">
+        <small>
+          © 2024 Design & Dveloped By{" "}
+          <a
+            to="https://www.goinnovior.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#83bf24]"
+          >
+            Goinnovior Limited
+          </a>
+        </small>
+      </div>
     </div>
   );
 };
